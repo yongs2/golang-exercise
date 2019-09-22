5d22046186e7d093c1ea5313fbfd5af5e7aea7c7
yong-sik Lee <yongs2yj@gmail.com>
2019-09-22 21:30:27 +0900

    add example : constant

--- 01.hello/hello.go
+++ 01.hello/hello.go
@@ -10,17 +10,39 @@ func main() {
 	var s3 string = "한글"
 
 	fmt.Println("hello world !!!")
-	fmt.Println(len(s1));	// 15
-	fmt.Println(len(s2));	// 16
-	fmt.Println(len(s3));	// 6 (unicode 이므로 한글 1자가 3bytes 로 처리됨)
-	fmt.Println(utf8.RuneCountInString(s3));	// 2
+	fmt.Println(len(s1))	// 15
+	fmt.Println(len(s2))	// 16
+	fmt.Println(len(s3))	// 6 (unicode 이므로 한글 1자가 3bytes 로 처리됨)
+	fmt.Println(utf8.RuneCountInString(s3))	// 2
 
 	s1 = "Hello world ~ Wonderful"
 	fmt.Println(s1)
 
+	// String operation
 	s1 = "Hello"
 	s2 = "Hello"
 	s3 = "World"
 	fmt.Println(s1 == s2)
 	fmt.Println(s1+s3)
+
+	// boolean
+	var V1 bool = true
+	var V2 bool = false
+	fmt.Println(V1, V2)
+
+	// const
+	const a1 = 10
+	const b1 = 20
+	fmt.Println(a1, b1)
+
+	// 열거형
+	const (
+		a=iota	// 0
+		b
+		c
+		d
+		e
+		f	
+	)
+	fmt.Println(a, b, c, d, e, f)
 }
