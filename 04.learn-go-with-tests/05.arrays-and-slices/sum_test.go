ce00fd9f3b51afc1a24579057390a2f1e445cd74
yongs2 <yongs2@hotmail.com>
2019-11-01 02:10:52 +0000

    fix SumAll

--- 04.learn-go-with-tests/05.arrays-and-slices/sum_test.go
+++ 04.learn-go-with-tests/05.arrays-and-slices/sum_test.go
@@ -32,7 +32,7 @@ func TestSum(t *testing.T) {
 
 func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1,2}, []int{0,9})
-	want := "bob" // []int{3, 9}
+	want := []int{3, 9}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
