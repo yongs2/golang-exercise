70a559ad0f351fed1e39eada641df8a79248e936
yongs2 <yongs2@hotmail.com>
2019-11-01 01:34:31 +0000

    add Benchmark

--- 04.learn-go-with-tests/04.interation/repeat_test.go
+++ 04.learn-go-with-tests/04.interation/repeat_test.go
@@ -10,3 +10,10 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
+
+// go test -bench=.
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
