64cebe5df17b7e85b520e7727469a16736670dd4
yongs2 <yongs2@hotmail.com>
2019-11-01 00:57:09 +0000

    add greetingPrefix

--- 04.learn-go-with-tests/02.hello-world/hello.go
+++ 04.learn-go-with-tests/02.hello-world/hello.go
@@ -13,16 +13,19 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+	return greetingPrefix(language) + name
+}
 
-	prefix := englishHelloPrefix
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case spanish :
 		prefix = spanishHelloPrefix
 	case french :
 		prefix = frenchHelloPrefix
+	default :
+		prefix = englishHelloPrefix
 	}
-	
-	return prefix + name
+	return
 }
 
 func main() {
