2bb5d9d14ab4bb9fd974ba4f65da90d9466b0374
yongs2 <yongs2@hotmail.com>
2019-11-01 01:32:37 +0000

    refactor Repeat

--- 04.learn-go-with-tests/04.interation/repeat.go
+++ 04.learn-go-with-tests/04.interation/repeat.go
@@ -1,10 +1,12 @@
 package interation
 
+const repeatCount = 5
+
 func Repeat(character string) string {
 	var repeated string
 
-	for i := 0; i < 5; i++ {
-		repeated = repeated + character
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
 	}
 	return repeated
 }
