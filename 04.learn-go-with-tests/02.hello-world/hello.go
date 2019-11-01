44649973a41c40a9e43b303f038bef45b6dd0eb9
yongs2 <yongs2@hotmail.com>
2019-11-01 00:51:10 +0000

    add spanish

--- 04.learn-go-with-tests/02.hello-world/hello.go
+++ 04.learn-go-with-tests/02.hello-world/hello.go
@@ -4,13 +4,16 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == "Spanish" {
+		return "Hola, " + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("", "english"))
 }
--- 04.learn-go-with-tests/02.hello-world/hello_test.go
+++ 04.learn-go-with-tests/02.hello-world/hello_test.go
@@ -11,14 +11,20 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "english")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "english")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
