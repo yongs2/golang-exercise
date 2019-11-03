b5e306aac9e5ef735e11db33dceb0190357f7725
yongs2 <yongs2@hotmail.com>
2019-11-03 12:12:56 +0000

    change Bitcoin

--- 04.learn-go-with-tests/07.pointers-and-errors/wallet.go
+++ 04.learn-go-with-tests/07.pointers-and-errors/wallet.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 )
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
