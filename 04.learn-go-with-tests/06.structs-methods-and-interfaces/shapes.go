cdc51574e8520ee7eab9a3d06a05d631077c7e77
yongs2 <yongs2@hotmail.com>
2019-11-03 11:49:03 +0000

    change interface

--- 04.learn-go-with-tests/06.structs-methods-and-interfaces/shapes.go
+++ 04.learn-go-with-tests/06.structs-methods-and-interfaces/shapes.go
@@ -2,6 +2,10 @@ package shapes
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -11,14 +15,14 @@ type Circle struct {
 	Radius float64
 }
 
-func (r *Rectangle) Perimeter() float64 {
+func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.Width + r.Height)
 }
 
-func (r *Rectangle) Area() float64 {
+func (r Rectangle) Area() float64 {
 	return (r.Width * r.Height)
 }
 
-func (c *Circle) Area() float64 {
+func (c Circle) Area() float64 {
 	return (math.Pi * (c.Radius * c.Radius))
 }
