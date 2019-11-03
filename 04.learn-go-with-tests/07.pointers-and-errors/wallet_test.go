124833dc4d3e05594f16612f491e68bdcb9785b9
yongs2 <yongs2@hotmail.com>
2019-11-03 12:49:01 +0000

    add assertNoError

--- 04.learn-go-with-tests/07.pointers-and-errors/wallet_test.go
+++ 04.learn-go-with-tests/07.pointers-and-errors/wallet_test.go
@@ -13,8 +13,10 @@ func TestWallet(t *testing.T) {
 	
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
@@ -42,5 +44,12 @@ func assertError(t *testing.T, got error, want error) {
 	}
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
