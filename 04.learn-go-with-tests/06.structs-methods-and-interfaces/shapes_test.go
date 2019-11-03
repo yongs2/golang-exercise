5fb2d67042e0445d20965831cb5ae0d8add48327
yongs2 <yongs2@hotmail.com>
2019-11-03 11:55:03 +0000

    add Triangle

--- 04.learn-go-with-tests/06.structs-methods-and-interfaces/shapes_test.go
+++ 04.learn-go-with-tests/06.structs-methods-and-interfaces/shapes_test.go
@@ -17,8 +17,9 @@ func TestArea(t *testing.T) {
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12, 6}, 72},
+		{Rectangle{12, 6}, 72.0},
 		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
 	}
 
 	for _, tt := range areaTests {
