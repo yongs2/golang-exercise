3e0b4fd56aaa7fbe4faccabd79965d6c9b4d8b97
yongs2 <yongs2@hotmail.com>
2019-11-03 12:11:09 +0000

    add 1st wallet

---
+++ 04.learn-go-with-tests/07.pointers-and-errors/wallet_test.go
@@ -0,0 +1,20 @@
+package wallet
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+
+	want := 10
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
