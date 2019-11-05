f230d34c5e4c9855ba4e23b7c473686a96e602d1
yongs2 <yongs2@hotmail.com>
2019-11-05 12:15:53 +0000

    refactor to add ErrWordExists

--- 04.learn-go-with-tests/08.maps/dictionary.go
+++ 04.learn-go-with-tests/08.maps/dictionary.go
@@ -6,7 +6,10 @@ import (
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -16,6 +19,15 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
 }
