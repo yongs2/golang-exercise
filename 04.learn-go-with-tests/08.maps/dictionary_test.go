bf8153c1a45ead3020fd6ac7a25f2801de1a3ac5
yongs2 <yongs2@hotmail.com>
2019-11-05 11:50:15 +0000

    Refactor to add assertString()

--- 04.learn-go-with-tests/08.maps/dictionary_test.go
+++ 04.learn-go-with-tests/08.maps/dictionary_test.go
@@ -4,12 +4,18 @@ import (
 	"testing"
 )
 
+func assertString(t *testing.T, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test" : "this is just a test"}
+	dictionary := map[string]string{"test": "this is just a test"}
 
 	got := Search(dictionary, "test")
 	want := "this is just a test"
-	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
-	}
+	assertString(t, got, want)
 }
