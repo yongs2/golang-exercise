f1a9ead5a228e8b563709f9a3fb6cef9909a0658
yongs2 <yongs2@hotmail.com>
2019-11-05 12:22:22 +0000

    add Update()

--- 04.learn-go-with-tests/08.maps/dictionary.go
+++ 04.learn-go-with-tests/08.maps/dictionary.go
@@ -32,3 +32,14 @@ func (d Dictionary) Add(word, definition string) error {
 	}
 	return nil
 }
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+		return nil
+	default:
+		return ErrNotFound
+	}
+}
--- 04.learn-go-with-tests/08.maps/dictionary_test.go
+++ 04.learn-go-with-tests/08.maps/dictionary_test.go
@@ -74,3 +74,13 @@ func assertDefinition(t *testing.T, dictionary Dictionary, word, definition stri
 		t.Errorf("got %q want %q", got, definition)
 	}
 }
+
+func TestUpdate(t *testing.T) {
+	word := "test"
+	definition := "this is just a test"
+	dictionary := Dictionary{word: definition}
+	newDefinition := "new definition"
+
+	dictionary.Update(word, newDefinition)
+	assertDefinition(t, dictionary, word, newDefinition)
+}
