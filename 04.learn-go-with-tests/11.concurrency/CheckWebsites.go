89151f12aeafcc48715b8d5967298d3dc747ff77
yongs2 <yongs2@hotmail.com>
2019-11-09 07:39:55 +0000

    add go routine and channel

--- 04.learn-go-with-tests/11.concurrency/CheckWebsites.go
+++ 04.learn-go-with-tests/11.concurrency/CheckWebsites.go
@@ -1,17 +1,28 @@
 package concurrency
 
 import (
-	//"fmt"
+//"fmt"
+//"time"
 )
 
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
-		//fmt.Println(url, "=", results[url])
+		go func(u string) { // go test -race
+			resultChannel <- result{u, wc(u)} // Send statement
+		}(url)
+	}
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel // Receive expression
+		results[result.string] = result.bool
 	}
 	return results
 }
