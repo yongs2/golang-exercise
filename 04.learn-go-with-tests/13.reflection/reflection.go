6a181da8a2ba7e643f1339aa7bdcb7b5a123ab9d
yongs2 <yongs2@hotmail.com>
2019-11-10 11:51:34 +0000

    refactor to update test case for multiple fields

--- 04.learn-go-with-tests/13.reflection/reflection.go
+++ 04.learn-go-with-tests/13.reflection/reflection.go
@@ -8,7 +8,10 @@ import (
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
 	fmt.Println("walk:", val)
-	field := val.Field(0)
-	fmt.Println("walk:", field)
-	fn(field.String())
+	
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fmt.Println("walk:", i, ":", field)
+		fn(field.String())
+	}
 }
