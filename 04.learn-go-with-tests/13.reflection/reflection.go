6aae2bc1001be6513d375cd37013795be4a750d4
yongs2 <yongs2@hotmail.com>
2019-11-10 13:02:44 +0000

    refactor to change to check type first

--- 04.learn-go-with-tests/13.reflection/reflection.go
+++ 04.learn-go-with-tests/13.reflection/reflection.go
@@ -8,28 +8,25 @@ import (
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	if val.Kind() == reflect.Slice {
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
+			fmt.Println("walk:", i, ":", val.Field(i), val.Field(i).Kind())
+		}
+	case reflect.Slice:
 		for i := 0; i < val.Len(); i++ {
 			walk(val.Index(i).Interface(), fn)
+			fmt.Println("walk:", i, ":", val.Index(i), val.Index(i).Kind())
 		}
-		return
-	}
-
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-		fmt.Println("walk:", i, ":", field, field.Kind())
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
-		}
+	case reflect.String:
+		fn(val.String())
 	}
 }
 
 func getValue(x interface{}) reflect.Value {
 	val := reflect.ValueOf(x)
-	fmt.Println("walk:", val)
+	fmt.Println("walk:getValue:", val)
 	if val.Kind() == reflect.Ptr {
 		val = val.Elem()
 	}
