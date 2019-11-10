69fc6f52a6a1606ea1cefbf5aae518de490727ce
yongs2 <yongs2@hotmail.com>
2019-11-10 13:12:27 +0000

    refactor to add Map

--- 04.learn-go-with-tests/13.reflection/reflection_test.go
+++ 04.learn-go-with-tests/13.reflection/reflection_test.go
@@ -77,6 +77,14 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavik"},
 		},
+		{
+			"Maps",
+			map[string]string {
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
 	}
 
 	for _, test := range cases {
