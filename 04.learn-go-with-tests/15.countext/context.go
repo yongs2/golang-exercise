9a8d97214a768b9e20b60c5f4c972938bb76639c
yongs2 <yongs2@hotmail.com>
2019-11-11 12:21:54 +0000

    add first commit

---
+++ 04.learn-go-with-tests/15.countext/context.go
@@ -0,0 +1,16 @@
+package context
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch() string
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, store.Fetch())
+	}
+}
---
+++ 04.learn-go-with-tests/15.countext/context_test.go
@@ -0,0 +1,29 @@
+package context
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+)
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
+
+func TestHandler(t *testing.T) {
+	data := "hello, world"
+	svr := Server(&StubStore{data})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	svr.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf(` got "%s", want "%s"`, response.Body.String(), data)
+	}
+}
