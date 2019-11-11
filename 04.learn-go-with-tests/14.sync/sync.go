e131edbea21b63df18f7fcfc7911f6da84ee8cdb
yongs2 <yongs2@hotmail.com>
2019-11-11 12:04:47 +0000

    refactor to add mutex

--- 04.learn-go-with-tests/14.sync/sync.go
+++ 04.learn-go-with-tests/14.sync/sync.go
@@ -1,14 +1,17 @@
 package sync
 
 import (
-
+	"sync"
 )
 
 type Counter struct {
+	mu sync.Mutex
 	value int
 }
 
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
--- 04.learn-go-with-tests/14.sync/sync_test.go
+++ 04.learn-go-with-tests/14.sync/sync_test.go
@@ -1,8 +1,8 @@
 package sync
 
 import (
-	"testing"
 	"sync"
+	"testing"
 )
 
 func TestCounter(t *testing.T) {
@@ -16,13 +16,13 @@ func TestCounter(t *testing.T) {
 	})
 
 	t.Run("it runs safely concurrently", func(t *testing.T) {
-		wantedCount := 1000
+		wantedCount := 10000	// mutext 를 쓰지 않으면, 큰 값으로 설정하는 경우 오류가 발생
 		counter := Counter{}
 
 		var wg sync.WaitGroup
 		wg.Add(wantedCount)
 
-		for i := 0; i<wantedCount; i++ {
+		for i := 0; i < wantedCount; i++ {
 			go func(w *sync.WaitGroup) {
 				counter.Inc()
 				w.Done()
