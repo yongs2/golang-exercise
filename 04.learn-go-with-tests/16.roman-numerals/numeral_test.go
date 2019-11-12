130c56c2288e529a59debb16ddc57a94abd656ee
yongs2 <yongs2@hotmail.com>
2019-11-12 12:10:15 +0000

    refactor to add test case 9

--- 04.learn-go-with-tests/16.roman-numerals/numeral_test.go
+++ 04.learn-go-with-tests/16.roman-numerals/numeral_test.go
@@ -15,6 +15,10 @@ func TestRomanNumerals(t *testing.T) {
 		{"3 gets convert to III", 3, "III"},
 		{"4 gets convert to IV", 4, "IV"},
 		{"5 gets convert to V", 5, "V"},
+		{"6 gets convert to VI", 6, "VI"},
+		{"7 gets convert to VII", 7, "VII"},
+		{"8 gets convert to VIII", 8, "VIII"},
+		{"9 gets convert to IX", 9, "IX"},
 	}
 
 	for _, test := range cases {
