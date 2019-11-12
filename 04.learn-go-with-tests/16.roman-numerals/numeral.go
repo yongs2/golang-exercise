bef8f0c6e7b5457fc268b4a5e093148b2d1d0947
yongs2 <yongs2@hotmail.com>
2019-11-12 12:04:56 +0000

    refactor to change for loop

--- 04.learn-go-with-tests/16.roman-numerals/numeral.go
+++ 04.learn-go-with-tests/16.roman-numerals/numeral.go
@@ -5,13 +5,13 @@ import (
 )
 
 func ConvertToRoman(arabic int) string {
-	if arabic == 4 {
-		return "IV"
-	}
-	
 	var result strings.Builder
 
-	for i := 0; i<arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 	return result.String()
