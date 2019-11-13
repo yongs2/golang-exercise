242349aadc685b2c9bc0e26243722e5d8c7ecdf5
yongs2 <yongs2@hotmail.com>
2019-11-13 03:34:19 +0000

    refactor to add TestSVGWriterAtMidnight

--- 04.learn-go-with-tests/17.maths/clockface.go
+++ 04.learn-go-with-tests/17.maths/clockface.go
@@ -3,6 +3,9 @@ package clockface
 import (
 	"time"
 	"math"
+	"io"
+	"fmt"
+	"encoding/xml"
 )
 
 type Point struct {
@@ -10,9 +13,11 @@ type Point struct {
 	Y float64
 }
 
-const secondHandLength = 90
-const clockCenterX = 150
-const clockCenterY = 150
+const (
+	secondHandLength = 90
+	clockCenterX = 150
+	clockCenterY = 150
+)
 
 func SecondHand(t time.Time) Point {
 	p := secondsHandPoint(t)
@@ -31,4 +36,53 @@ func secondsHandPoint(t time.Time) Point {
 	x := math.Sin(angle)
 	y := math.Cos(angle)
 	return Point{x, y}
+}
+
+func SVGWriter(w io.Writer, t time.Time) {
+	io.WriteString(w, svgStart)
+	io.WriteString(w, bezel)
+	secondHand(w, t)
+	io.WriteString(w, svgEnd)
+}
+
+func secondHand(w io.Writer, t time.Time) {
+	p := SecondHand(t)
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
+}
+
+const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="100%"
+     height="100%"
+     viewBox="0 0 300 300"
+     version="2.0">`
+
+const bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
+
+const svgEnd = `</svg>`
+
+type Svg struct {
+	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  struct {
+		Text  string `xml:",chardata"`
+		Cx    string `xml:"cx,attr"`
+		Cy    string `xml:"cy,attr"`
+		R     string `xml:"r,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"circle"`
+	Line []struct {
+		Text  string `xml:",chardata"`
+		X1    string `xml:"x1,attr"`
+		Y1    string `xml:"y1,attr"`
+		X2    string `xml:"x2,attr"`
+		Y2    string `xml:"y2,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"line"`
 }
--- 04.learn-go-with-tests/17.maths/clockface_test.go
+++ 04.learn-go-with-tests/17.maths/clockface_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 	"time"
 	"math"
+	"bytes"
+	"encoding/xml"
 )
 
 func TestSecondHandAtMidnight(t *testing.T) {
@@ -80,4 +82,23 @@ func roughlyEqualFloat64(a, b float64) bool {
 func roughlyEqualPoint(a, b Point) bool {
 	return roughlyEqualFloat64(a.X, b.X) &&
 			roughlyEqualFloat64(a.Y, b.Y)
+}
+
+func TestSVGWriterAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	b := bytes.Buffer{}
+	SVGWriter(&b, tm)
+
+	svg := Svg{}
+	xml.Unmarshal(b.Bytes(), &svg)
+
+	x2 := "150.000"
+	y2 := "60.000"
+	for _, line := range svg.Line {
+		if line.X2 == x2 && line.Y2 == y2 {
+			return
+		}
+	}
+	t.Errorf("Expected to find the second hand with x2 of %+v and y2 of %+v, in the SVG output %v", x2, y2, b.String())
 }
