e0bf76bfe1abe63b3468ac42f23b88ae703b95be
yongs2 <yongs2@hotmail.com>
2019-11-13 01:46:33 +0000

    add first commit

---
+++ 04.learn-go-with-tests/17.maths/clockface_test.go
@@ -0,0 +1,16 @@
+package clockface
+
+import (
+	"testing"
+	"time"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 - 90}
+	got := SecondHand(tm)
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
