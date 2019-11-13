0c75d5ed514805a4fa3b8c28503411cb5f75b9e1
yongs2 <yongs2@hotmail.com>
2019-11-13 03:55:50 +0000

    refactor to add TestSVGWriterSecondHand

--- 04.learn-go-with-tests/17.maths/clockface_test.go
+++ 04.learn-go-with-tests/17.maths/clockface_test.go
@@ -99,5 +99,44 @@ func TestSVGWriterAtMidnight(t *testing.T) {
 			return
 		}
 	}
-	t.Errorf("Expected to find the second hand line %+v, in the SVG output %+v", want, svg.Line)
+	t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", want, svg.Line)
+}
+
+func TestSVGWriterSecondHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(0, 0, 0),
+			Line{150, 150, 150, 60},
+		},
+		{
+			simpleTime(0, 0, 30),
+			Line{150, 150, 150, 240},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			b := bytes.Buffer{}
+			SVGWriter(&b, c.time)
+
+			svg := Svg{}
+			xml.Unmarshal(b.Bytes(), &svg)
+
+			if !containsLine(c.line, svg.Line) {
+				t.Errorf("Expected to find the second hand line %+v, in the SVG output %+v", c.line, svg.Line)
+			}
+		})
+	}
+}
+
+func containsLine(l Line, ls []Line) bool {
+	for _, line := range ls {
+		if line == l {
+			return true
+		}
+	}
+	return false
 }
