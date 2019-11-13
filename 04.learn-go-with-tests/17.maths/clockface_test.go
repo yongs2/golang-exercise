b7a078715d3534f019718400b3238ea2d922a8da
yongs2 <yongs2@hotmail.com>
2019-11-13 02:05:11 +0000

    refactor to add secondsHandPoint

--- 04.learn-go-with-tests/17.maths/clockface_test.go
+++ 04.learn-go-with-tests/17.maths/clockface_test.go
@@ -52,4 +52,21 @@ func simpleTime(hours, minutes, seconds int) time.Time {
 
 func testName(t time.Time) string {
 	return t.Format("15:04:05")
-} 
+}
+
+func TestSecondsHandVector(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		point Point
+	}{
+		{simpleTime(0, 0, 30), Point{0, -1}},
+	}
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondsHandPoint(c.time)
+			if got != c.point {
+				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
+			}
+		})
+	}
+}
