8adcbfbc307ae74442782183713961bb2d28646e
yongs2 <yongs2@hotmail.com>
2019-11-13 02:12:30 +0000

    refactor to change const

--- 04.learn-go-with-tests/17.maths/clockface.go
+++ 04.learn-go-with-tests/17.maths/clockface.go
@@ -10,11 +10,15 @@ type Point struct {
 	Y float64
 }
 
+const secondHandLength = 90
+const clockCenterX = 150
+const clockCenterY = 150
+
 func SecondHand(t time.Time) Point {
 	p := secondsHandPoint(t)
-	p = Point{p.X * 90, p.Y * 90}	// scale
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength}	// scale
 	p = Point{p.X, -p.Y}	// flip
-	p = Point{p.X + 150, p.Y + 150}	// translate
+	p = Point{p.X + clockCenterX, p.Y + clockCenterY}	// translate
 	return p
 }
 
