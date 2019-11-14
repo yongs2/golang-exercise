218407af8b4a32dde2e3f26939de9469404fa6e6
yongs2 <yongs2@hotmail.com>
2019-11-14 12:20:53 +0000

    refactor to add test case for POST

--- 04.learn-go-with-tests/18.http-server/server_test.go
+++ 04.learn-go-with-tests/18.http-server/server_test.go
@@ -55,6 +55,21 @@ func TestGETPlayers(t *testing.T) {
 	})
 }
 
+func TestStoreWins(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{},
+	}
+	server := &PlayerServer{&store}
+
+	t.Run("it returns accepted on POST", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/Player/Pepper", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+		assertStatus(t, response.Code, http.StatusAccepted)
+	})
+}
+
 func assertStatus(t *testing.T, got, want int) {
 	t.Helper()
 	if got != want {
