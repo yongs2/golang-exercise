6ce45c498071e47d0666806971c2d3c7c7dbc82b
yongs2 <yongs2@hotmail.com>
2019-11-15 00:25:55 +0000

    refactor to add NewPlayerServer

--- 04.learn-go-with-tests/19.json/main.go
+++ 04.learn-go-with-tests/19.json/main.go
@@ -24,7 +24,7 @@ func (i *InMemoryPlayerStore) RecordWin(name string) {
 // curl -X POST http://localhost:5000/players/Pepper
 // curl http://localhost:5000/players/Pepper
 func main() {
-	server := &PlayerServer{NewInMemoryPlayerStore()}
+	server := NewPlayerServer(NewInMemoryPlayerStore())
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
--- 04.learn-go-with-tests/19.json/server_test.go
+++ 04.learn-go-with-tests/19.json/server_test.go
@@ -29,7 +29,7 @@ func TestGETPlayers(t *testing.T) {
 		},
 		nil,
 	}
-	server := &PlayerServer{&store}
+	server := NewPlayerServer(&store)
 
 	t.Run("returns Pepper's score", func(t *testing.T) {
 		request := newGetScoreRequest("Pepper")
@@ -66,7 +66,7 @@ func TestStoreWins(t *testing.T) {
 		map[string]int{},
 		nil,
 	}
-	server := &PlayerServer{&store}
+	server := NewPlayerServer(&store)
 
 	t.Run("it returns accepted on POST", func(t *testing.T) {
 		store.winCalls = nil
@@ -103,7 +103,7 @@ func TestStoreWins(t *testing.T) {
 
 func TestLeague(t *testing.T) {
 	store := StubPlayerStore{}
-	server := &PlayerServer{&store}
+	server := NewPlayerServer(&store)
 
 	t.Run("it returns 200 on /league", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
