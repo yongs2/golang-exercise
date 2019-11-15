bfc6218870baf2437c4b64a4abfa8793a8065035
yongs2 <yongs2@hotmail.com>
2019-11-15 00:30:23 +0000

    refactor to change from router to Handler

--- 04.learn-go-with-tests/19.json/server.go
+++ 04.learn-go-with-tests/19.json/server.go
@@ -12,22 +12,20 @@ type PlayerStore interface {
 
 type PlayerServer struct {
 	store PlayerStore
-	router *http.ServeMux
+	http.Handler
 }
 
 func NewPlayerServer(store PlayerStore) *PlayerServer {
-	p := &PlayerServer{
-		store,
-		http.NewServeMux(),
-	}
-	p.router.Handle("/league", http.HandlerFunc(p.leagueHandler))
-	p.router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+	p := new(PlayerServer)
+
+	p.store = store
+
+	router := http.NewServeMux()
+	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
+	router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+	p.Handler = router
 
 	return p
-}
-
-func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	p.router.ServeHTTP(w, r)
 }
 
 func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
