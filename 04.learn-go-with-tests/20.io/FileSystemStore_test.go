8d0ad849aa7fa5cde04a1d2a9af1cf91e2d9c558
yongs2 <yongs2@hotmail.com>
2019-11-16 14:09:01 +0000

    refactor to change NewFileSystemPlayerStore

--- 04.learn-go-with-tests/20.io/FileSystemStore_test.go
+++ 04.learn-go-with-tests/20.io/FileSystemStore_test.go
@@ -14,7 +14,7 @@ func TestFileSystemStore(t *testing.T) {
 			{"Name":"Chris", "Wins":33}]`)
 		defer cleanDatabase()
 
-		store := FileSystemPlayerStore{database}
+		store := NewFileSystemPlayerStore(database)
 
 		got := store.GetLeague()
 		want := []Player{
@@ -34,7 +34,7 @@ func TestFileSystemStore(t *testing.T) {
 			{"Name":"Chris", "Wins":33}]`)
 		defer cleanDatabase()
 
-		store := FileSystemPlayerStore{database}
+		store := NewFileSystemPlayerStore(database)
 
 		got := store.GetPlayerScore("Chris")
 		want := 33
@@ -47,7 +47,7 @@ func TestFileSystemStore(t *testing.T) {
 			{"Name":"Chris", "Wins":33}]`)
 		defer cleanDatabase()
 
-		store := FileSystemPlayerStore{database}
+		store := NewFileSystemPlayerStore(database)
 
 		store.RecordWin("Chris")
 
@@ -62,7 +62,7 @@ func TestFileSystemStore(t *testing.T) {
 			{"Name":"Chris", "Wins":33}]`)
 		defer cleanDatabase()
 
-		store := FileSystemPlayerStore{database}
+		store := NewFileSystemPlayerStore(database)
 
 		store.RecordWin("Pepper")
 
