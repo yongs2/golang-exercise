1a0f9558647ab22ec667af65848f62a1c2695550
yongs2 <yongs2@hotmail.com>
2019-11-16 13:44:59 +0000

    refactor to handle new players

--- 04.learn-go-with-tests/20.io/FileSystemStore.go
+++ 04.learn-go-with-tests/20.io/FileSystemStore.go
@@ -29,6 +29,8 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 	player := league.Find(name)
 	if player != nil {
 		player.Wins++
+	} else {
+		league = append(league, Player{name, 1})
 	}
 
 	f.database.Seek(0, 0)
