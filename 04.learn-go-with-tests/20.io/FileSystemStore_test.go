454eac06084131ea02b88fbc56736b671fa59d27
yongs2 <yongs2@hotmail.com>
2019-11-16 13:42:06 +0000

    refactor to add League.Find

--- 04.learn-go-with-tests/20.io/FileSystemStore_test.go
+++ 04.learn-go-with-tests/20.io/FileSystemStore_test.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"testing"
 	"io"
 	"io/ioutil"
 	"os"
+	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
--- 04.learn-go-with-tests/20.io/league.go
+++ 04.learn-go-with-tests/20.io/league.go
@@ -1,14 +1,24 @@
 package main
 
 import (
-	"io"
 	"encoding/json"
 	"fmt"
+	"io"
 )
 
-func NewLeague(rdr io.Reader) ([]Player, error) {
-	var league []Player
+type League []Player
 
+func (l League) Find(name string) *Player {
+	for i, p := range l {
+		if p.Name == name {
+			return &l[i]
+		}
+	}
+	return nil
+}
+
+func NewLeague(rdr io.Reader) (League, error) {
+	var league League
 	err := json.NewDecoder(rdr).Decode(&league)
 	if err != nil {
 		err = fmt.Errorf("problem parsing league, %v", err)
