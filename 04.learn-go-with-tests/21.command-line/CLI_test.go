375d13404639607e50ac9c6a51ebb262211aa737
yongs2 <yongs2@hotmail.com>
2019-11-19 12:55:09 +0000

    refactor to add CLI test case

--- 04.learn-go-with-tests/21.command-line/CLI_test.go
+++ 04.learn-go-with-tests/21.command-line/CLI_test.go
@@ -2,14 +2,24 @@ package poker
 
 import (
 	"testing"
+	"strings"
 )
 
 func TestCLI(t *testing.T) {
+	in := strings.NewReader("Chris wins\n")
 	playerStore := &StubPlayerStore{}
-	cli := &CLI{playerStore}
+
+	cli := &CLI{playerStore, in}
 	cli.PlayPoker()
 
-	if len(playerStore.winCalls) != 1 {
+	if len(playerStore.winCalls) < 1 {
 		t.Fatal("expected a win call but didn't get any")
+	}
+
+	got := playerStore.winCalls[0]
+	want := "Chris"
+
+	if got != want {
+		t.Errorf("didn't record correct winner, got %q, want %q", got, want)
 	}
 }
