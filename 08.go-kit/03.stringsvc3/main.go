9fd477fe6b91df793b77f9efe2520ccea15cc33b
yongs2 <yongs2@hotmail.com>
2019-12-22 13:59:24 +0000

    add go-kit/stringsvc3

---
+++ 08.go-kit/03.stringsvc3/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"net/http"
+	"os"
+
+	"github.com/go-kit/kit/log"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
+	httptransport "github.com/go-kit/kit/transport/http"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+)
+
+func main() {
+	var (
+		listen = flag.String("listen", ":8080", "HTTP Listen address")
+		proxy  = flag.String("proxy", "", "Optional comma-separated list of URLs to proxy uppercase requests")
+	)
+	flag.Parse()
+
+	var logger log.Logger
+	logger = log.NewLogfmtLogger(os.Stderr)
+	logger = log.With(logger, "listen", *listen, "caller", log.DefaultCaller)
+
+	fieldKeys := []string{"method", "error"}
+	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "request_count",
+		Help:      "Number of requests received.",
+	}, fieldKeys)
+	requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "request_latency_microseconds",
+		Help:      "Total duration of requests in microseconds.",
+	}, fieldKeys)
+	countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "count_result",
+		Help:      "The result of each count method.",
+	}, []string{}) // no fields here
+
+	var svc StringService
+	svc = stringService{}
+	svc = proxyingMiddleware(context.Background(), *proxy, logger)(svc)
+	svc = loggingMiddleware(logger)(svc)
+	svc = instrumentingMiddleware(requestCount, requestLatency, countResult)(svc)
+
+	uppercaseHandler := httptransport.NewServer(
+		makeUppercaseEndpoint(svc),
+		decodeUppercaseRequest,
+		encodeResponse,
+	)
+
+	countHandler := httptransport.NewServer(
+		makeCountEndpoint(svc),
+		decodeCountRequest,
+		encodeResponse,
+	)
+
+	http.Handle("/uppercase", uppercaseHandler)
+	http.Handle("/count", countHandler)
+	http.Handle("/metrics", promhttp.Handler())
+	logger.Log("msg", "HTTP", "addr", ":8080")
+	logger.Log("err", http.ListenAndServe(":8080", nil))
+}
--- 08.go-kit/README.md
+++ 08.go-kit/README.md
@@ -16,7 +16,22 @@ curl -XPOST -d'{"s":"hello, world"}' localhost:8080/count
 ### stringsvc2
 
 ```sh
-go run 02.stringsvc1/*.go
+go run 02.stringsvc2/*.go
+
+curl -XPOST -d'{"s":"hello, world"}' localhost:8080/uppercase
+curl -XPOST -d'{"s":"hello, world"}' localhost:8080/count
+```
+
+### stringsvc3
+
+```sh
+go get github.com/streadway/handy/breaker
+go get github.com/afex/hystrix-go/hystrix
+go get golang.org/x/time/rate
+```
+
+```sh
+go run 02.stringsvc3/*.go
 
 curl -XPOST -d'{"s":"hello, world"}' localhost:8080/uppercase
 curl -XPOST -d'{"s":"hello, world"}' localhost:8080/count
