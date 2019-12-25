9c867aaec1512a8ee12dce65ae489ecdd7dd5eeb
yongs2 <yongs2@hotmail.com>
2019-12-25 13:43:41 +0000

    add addsvc

---
+++ 08.go-kit/04.addsvc/cmd/addsvc/addsvc.go
@@ -0,0 +1,280 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"text/tabwriter"
+
+	"google.golang.org/grpc"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/metrics"
+	"github.com/go-kit/kit/metrics/prometheus"
+	kitgrpc "github.com/go-kit/kit/transport/grpc"
+
+	"github.com/apache/thrift/lib/go/thrift"
+	"github.com/oklog/oklog/pkg/group"
+
+	stdopentracing "github.com/opentracing/opentracing-go"
+
+	zipkinot "github.com/openzipkin-contrib/zipkin-go-opentracing"
+	zipkin "github.com/openzipkin/zipkin-go"
+	zipkinhttp "github.com/openzipkin/zipkin-go/reporter/http"
+
+	lightstep "github.com/lightstep/lightstep-tracer-go"
+
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"sourcegraph.com/sourcegraph/appdash"
+	appdashot "sourcegraph.com/sourcegraph/appdash/opentracing"
+
+	addpb "08.go-kit/04.addsvc/pb"
+	"08.go-kit/04.addsvc/pkg/addendpoint"
+	"08.go-kit/04.addsvc/pkg/addservice"
+	"08.go-kit/04.addsvc/pkg/addtransport"
+	addthrift "08.go-kit/04.addsvc/thrift/gen-go/addsvc"
+)
+
+func main() {
+	fs := flag.NewFlagSet("addsvc", flag.ExitOnError)
+
+	var (
+		debugAddr      = fs.String("debug.addr", ":8080", "Debug and metrics listen address")
+		httpAddr       = fs.String("http-addr", ":8081", "HTTP listen address")
+		grpcAddr       = fs.String("grpc-addr", ":8082", "gRPC listen address")
+		thriftAddr     = fs.String("thrift-addr", ":8083", "Thrift listen address")
+		jsonRPCAddr    = fs.String("jsonrpc-addr", ":8084", "JSON RPC listen address")
+		thriftProtocol = fs.String("thrift-protocol", "binary", "binary, compact, json, simplejson")
+		thriftBuffer   = fs.Int("thrift-buffer", 0, "0 for unbuffered")
+		thriftFramed   = fs.Bool("thrift-framed", false, "true to enable framing")
+		zipkinURL      = fs.String("zipkin-url", "", "Enable Zipkin tracing via HTTP reporter URL e.g. http://localhost:9411/api/v2/spans")
+		zipkinBridge   = fs.Bool("zipkin-ot-bridge", false, "Use Zipkin OpenTracing bridge instead of native implementation")
+		lightstepToken = fs.String("lightstep-token", "", "Enable LightStep tracing via a LightStep access token")
+		appdashAddr    = fs.String("appdash-addr", "", "Enable Appdash tracing via an Appdash server host:port")
+	)
+	fs.Usage = usageFor(fs, os.Args[0]+" [flags]")
+	fs.Parse(os.Args[1:])
+
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+		logger = log.With(logger, "caller", log.DefaultCaller)
+	}
+
+	var zipkinTracer *zipkin.Tracer
+	{
+		if *zipkinURL != "" {
+			var (
+				err         error
+				hostPort    = "localhost:80"
+				serviceName = "addsvc"
+				reporter    = zipkinhttp.NewReporter(*zipkinURL)
+			)
+			defer reporter.Close()
+			zEP, _ := zipkin.NewEndpoint(serviceName, hostPort)
+			zipkinTracer, err = zipkin.NewTracer(reporter, zipkin.WithLocalEndpoint(zEP))
+			if err != nil {
+				logger.Log("err", err)
+				os.Exit(1)
+			}
+			if !(*zipkinBridge) {
+				logger.Log("tracer", "Zipkin", "type", "Native", "URL", *zipkinURL)
+			}
+		}
+	}
+
+	var tracer stdopentracing.Tracer
+	{
+		if *zipkinBridge && zipkinTracer != nil {
+			logger.Log("tracer", "Zipkin", "type", "OpenTracing", "URL", *zipkinURL)
+			tracer = zipkinot.Wrap(zipkinTracer)
+			zipkinTracer = nil
+		} else if *lightstepToken != "" {
+			logger.Log("tracer", "LightStep")
+			tracer = lightstep.NewTracer(lightstep.Options{
+				AccessToken: *lightstepToken,
+			})
+			defer lightstep.FlushLightStepTracer(tracer)
+		} else if *appdashAddr != "" {
+			logger.Log("tracer", "Appdash", "addr", *appdashAddr)
+			tracer = appdashot.NewTracer(appdash.NewRemoteCollector(*appdashAddr))
+		} else {
+			tracer = stdopentracing.GlobalTracer()
+		}
+	}
+
+	var ints, chars metrics.Counter
+	{
+		ints = prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "example",
+			Subsystem: "addsvc",
+			Name:      "integers_summed",
+			Help:      "Total count of integers summed via the Sum method.",
+		}, []string{})
+		chars = prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "example",
+			Subsystem: "addsvc",
+			Name:      "characters_concatenated",
+			Help:      "Total count of characters concatenated via the Concat method.",
+		}, []string{})
+	}
+	var duration metrics.Histogram
+	{
+		duration = prometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+			Namespace: "example",
+			Subsystem: "addsvc",
+			Name:      "request_duration_seconds",
+			Help:      "Request duration in seconds.",
+		}, []string{"method", "success"})
+	}
+	http.DefaultServeMux.Handle("/metrics", promhttp.Handler())
+
+	var (
+		service        = addservice.New(logger, ints, chars)
+		endpoints      = addendpoint.New(service, logger, duration, tracer, zipkinTracer)
+		httpHandler    = addtransport.NewHTTPHandler(endpoints, tracer, zipkinTracer, logger)
+		grpcServer     = addtransport.NewGRPCServer(endpoints, tracer, zipkinTracer, logger)
+		thriftServer   = addtransport.NewTriftServer(endpoints)
+		jsonrpcHandler = addtransport.NewJSONRPCHandler(endpoints, logger)
+	)
+
+	var g group.Group
+	{
+		debugListener, err := net.Listen("tcp", *debugAddr)
+		if err != nil {
+			logger.Log("transport", "debug/HTTP", "during", "Listen", "err", err)
+			os.Exit(1)
+		}
+		g.Add(func() error {
+			logger.Log("transport", "debug/HTTP", "addr", *debugAddr)
+			return http.Serve(debugListener, http.DefaultServeMux)
+		}, func(error) {
+			debugListener.Close()
+		})
+	}
+
+	{
+		httpListener, err := net.Listen("tcp", *httpAddr)
+		if err != nil {
+			logger.Log("transport", "HTTP", "during", "Listen", "err", err)
+			os.Exit(1)
+		}
+		g.Add(func() error {
+			logger.Log("transport", "HTTP", "addr", *httpAddr)
+			return http.Serve(httpListener, httpHandler)
+		}, func(error) {
+			httpListener.Close()
+		})
+	}
+
+	{
+		grpcListener, err := net.Listen("tcp", *grpcAddr)
+		if err != nil {
+			logger.Log("transport", "gRPC", "during", "Listen", "err", err)
+			os.Exit(1)
+		}
+		g.Add(func() error {
+			logger.Log("transport", "gRPC", "addr", *grpcAddr)
+			baseServer := grpc.NewServer(grpc.UnaryInterceptor(kitgrpc.Interceptor))
+			addpb.RegisterAddServer(baseServer, grpcServer)
+			return baseServer.Serve(grpcListener)
+		}, func(error) {
+			grpcListener.Close()
+		})
+	}
+
+	{
+		thriftSocket, err := thrift.NewTServerSocket(*thriftAddr)
+		if err != nil {
+			logger.Log("transport", "Thrift", "during", "Listen", "err", err)
+			os.Exit(1)
+		}
+		g.Add(func() error {
+			logger.Log("transport", "Thrift", "addr", *thriftAddr)
+			var protocolFactory thrift.TProtocolFactory
+			switch *thriftProtocol {
+			case "binary":
+				protocolFactory = thrift.NewTBinaryProtocolFactoryDefault()
+			case "compact":
+				protocolFactory = thrift.NewTCompactProtocolFactory()
+			case "json":
+				protocolFactory = thrift.NewTJSONProtocolFactory()
+			case "simplejson":
+				protocolFactory = thrift.NewTSimpleJSONProtocolFactory()
+			default:
+				return fmt.Errorf("invalid Thrift protocol %q", *thriftProtocol)
+			}
+
+			var transportFactory thrift.TTransportFactory
+			if *thriftBuffer > 0 {
+				transportFactory = thrift.NewTBufferedTransportFactory(*thriftBuffer)
+			} else {
+				transportFactory = thrift.NewTTransportFactory()
+			}
+			if *thriftFramed {
+				transportFactory = thrift.NewTFramedTransportFactory(transportFactory)
+			}
+			return thrift.NewTSimpleServer4(
+				addthrift.NewAddServiceProcessor(thriftServer),
+				thriftSocket,
+				transportFactory,
+				protocolFactory,
+			).Serve()
+		}, func(error) {
+			thriftSocket.Close()
+		})
+	}
+
+	{
+		httpListener, err := net.Listen("tcp", *jsonRPCAddr)
+		if err != nil {
+			logger.Log("transport", "JSONRPC over HTTP", "during", "Listen", "err", err)
+			os.Exit(1)
+		}
+		g.Add(func() error {
+			logger.Log("transport", "JSONRPC over HTTP", "addr", *jsonRPCAddr)
+			return http.Serve(httpListener, jsonrpcHandler)
+		}, func(error) {
+			httpListener.Close()
+		})
+	}
+
+	{
+		cancelInterrupt := make(chan struct{})
+		g.Add(func() error {
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+			select {
+			case sig := <-c:
+				return fmt.Errorf("received signal %s", sig)
+			case <-cancelInterrupt:
+				return nil
+			}
+		}, func(error) {
+			close(cancelInterrupt)
+		})
+	}
+	logger.Log("exit", g.Run())
+}
+
+func usageFor(fs *flag.FlagSet, short string) func() {
+	return func() {
+		fmt.Fprintf(os.Stderr, "USAGE\n")
+		fmt.Fprintf(os.Stderr, " %s\n", short)
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "FLAGS\n")
+		w := tabwriter.NewWriter(os.Stderr, 0, 2, 2, ' ', 0)
+		fs.VisitAll(func(f *flag.Flag) {
+			fmt.Fprintf(w, "\t-%s %s\t%s\n", f.Name, f.DefValue, f.Usage)
+		})
+		w.Flush()
+		fmt.Fprintf(os.Stderr, "\n")
+	}
+}
