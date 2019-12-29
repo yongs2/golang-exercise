0ec2eecb3fe26e1add239132f7c19c0f552bb02e
yongs2 <yongs2@hotmail.com>
2019-12-29 12:40:27 +0000

    add profilesvc

---
+++ 08.go-kit/05.profilesvc/cmd/profilesvc/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-kit/kit/log"
+
+	"08.go-kit/05.profilesvc"
+)
+
+func main() {
+	var (
+		httpAddr = flag.String("http.addr", ":8080", "HTTP Listen address")
+	)
+	flag.Parse()
+
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+		logger = log.With(logger, "caller", log.DefaultCaller)
+	}
+
+	var s profilesvc.Service
+	{
+		s = profilesvc.NewInmemService()
+		s = profilesvc.LoggingMiddleWare(logger)(s)
+	}
+
+	var h http.Handler
+	{
+		h = profilesvc.MakeHTTPHandler(s, log.With(logger, "component", "HTTP"))
+	}
+
+	errs := make(chan error)
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	go func() {
+		logger.Log("transport", "HTTP", "addr", *httpAddr)
+		errs <- http.ListenAndServe(*httpAddr, h)
+	}()
+
+	logger.Log("exit", <-errs)
+}
