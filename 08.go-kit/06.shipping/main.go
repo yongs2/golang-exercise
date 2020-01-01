8d9a90a6780d5dc1c37d8665df18473b93c7e77c
yongs2 <yongs2@hotmail.com>
2020-01-01 12:08:26 +0000

    Add shipping/main

---
+++ 08.go-kit/06.shipping/main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/go-kit/kit/log"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
+
+	"08.go-kit/06.shipping/booking"
+)
+
+const (
+	defaultPort              = "8080"
+	defaultRoutingServiceURL = "http://localhost:7878"
+)
+
+func main() {
+	var (
+		addr  = envString("PORT", defaultPort)
+		rsurl = envString("ROUTINGSERVICE_URL", defaultRoutingServiceURL)
+
+		httpAddr          = flag.String("http.addr", ":"+addr, "HTTP listen address")
+		routingServiceURL = flag.String("service.routing", rsurl, "routing service URL")
+
+		ctx = context.Background()
+	)
+
+	flag.Parse()
+
+	var logger log.Logger
+	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+
+	var (
+		cargos         = inmem.NewCargoRepository()
+		locations      = inmem.NewLocationRepository()
+		voyages        = inmem.NewVoyageRepository()
+		handlingEvents = inmem.NewHandlingEventRepository()
+	)
+
+	storeTestData(cargos)
+
+	filedKeys := []string{"method"}
+
+	var rs routing.Service
+	rs = routing.NewProxyingMiddleware(ctx, *routingServiceURL)(rs)
+
+	var bs booking.Service
+	bs = booking.NewService(cargos, locations, handlingEvents, rs)
+	bs = booking.NewLoggingService(log.With(logger, "component", "booking"), bs)
+	bs = booking.NewInstrumentingService(
+		kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "api",
+			Subsystem: "booking_service",
+			Name:      "request_count",
+			Help:      "Number of request recevied.",
+		}, filedKeys),
+		kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "api",
+			Subsystem: "booking_service",
+			Name:      "request_latency_microseconds",
+			Help:      "Total duration of requests in microseconds.",
+		}, filedKeys),
+		bs,
+	)
+
+	httpLogger := log.With(logger, "component", "http")
+	mux := http.NewServeMux()
+
+	mux.Handle("/booking/v1/", booking.MakeHandler(bs, httpLogger))
+
+	http.Handle("/", accessControl(mux))
+	http.Handle("/metrics", promhttp.Handler())
+
+	errs := make(chan error, 2)
+	go func() {
+		logger.Log("transport", "http", "address", *httpAddr, "msg", "listening")
+		errs <- http.ListenAndServe(*httpAddr, nil)
+	}()
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+	logger.Log("terminated", <-errs)
+}
+
+func accessControl(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func envString(env, fallback string) string {
+	e := os.Getenv(env)
+	if e == "" {
+		return fallback
+	}
+	return e
+}
+
+func storeTestData(r Cargo.Repository) {
+	test1 := cargo.New("FTL456", cargo.RouteSpecification{
+		Origin:         location.AUML,
+		Destination:    location.SESTO,
+		ArriveDeadline: time.Now().AddDate(0, 0, 7),
+	})
+	if err := r.Store(test1); err != nil {
+		panic(err)
+	}
+
+	test2 := cargo.New("ABC123", cargo.RouteSpecification{
+		Origin:         location.SESTO,
+		Destination:    location.CNHKG,
+		ArriveDeadline: time.Now().AddDate(0, 0, 14),
+	})
+	if err := r.Store(test2); err != nil {
+		panic(err)
+	}
+}
