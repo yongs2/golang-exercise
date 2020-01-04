e709b7824bc831ee5b141c6a2438946e639f5e3c
yongs2 <yongs2@hotmail.com>
2020-01-04 11:35:10 +0000

    add voyage

---
+++ 08.go-kit/06.shipping/voyage/voyage.go
@@ -0,0 +1,36 @@
+package voyage
+
+import (
+	"errors"
+	"time"
+
+	"08.go-kit/06.shipping/location"
+)
+
+type Number string
+
+type Voyage struct {
+	Number   Number
+	Schedule Schedule
+}
+
+func New(n Number, s Schedule) *Voyage {
+	return &Voyage{Number: n, Schedule: s}
+}
+
+type Schedule struct {
+	CarrierMovements []CarrierMovement
+}
+
+type CarrierMovement struct {
+	DepartureLocation location.UNLocode
+	ArriavelLocation  location.UNLocode
+	DepartureTime     time.Time
+	ArrivalTime       time.Time
+}
+
+var ErrUnknown = errors.New("unknown voyage")
+
+type Repository interface {
+	Find(Number) (*Voyage, error)
+}
