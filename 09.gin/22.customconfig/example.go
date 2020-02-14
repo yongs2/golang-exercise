9b639a6247ffa2b8c1ca8709a47cd4d87f74a7b3
yongs2 <yongs2@hotmail.com>
2020-02-14 12:05:30 +0000

    add custom http configuration

--- 09.gin/22.customconfig/example.go
+++ 09.gin/22.customconfig/example.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,5 +10,18 @@ import (
 func main() {
 	router := gin.Default()
 
-	http.ListenAndServe(":8080", router)
+	router.GET("/", func(c *gin.Context) {
+		time.Sleep(5 * time.Second)
+
+		c.JSON(http.StatusOK, gin.H{"status" : c.Request.URL.Path})
+	})
+
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 }
