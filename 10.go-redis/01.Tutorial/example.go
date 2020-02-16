068e802ba509b80de25327d90052d4eace2186d5
yongs2 <yongs2@hotmail.com>
2020-02-16 09:07:43 +0000

    add example to using json

--- 10.go-redis/01.Tutorial/example.go
+++ 10.go-redis/01.Tutorial/example.go
@@ -1,37 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/go-redis/redis"
 )
 
+// Author ...
+type Author struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
 func main() {
 	fmt.Println("Go Redis Tutorial")
-	
+
 	redisAddr := os.Getenv("REDIS_HOST") + ":" + os.Getenv("REDIS_PORT")
 	fmt.Println("REDIS=", redisAddr)
 
 	client := redis.NewClient(&redis.Options{
-		Addr: redisAddr,
+		Addr:     redisAddr,
 		Password: "",
-		DB: 0,
+		DB:       0,
 	})
 
 	// ping, pong
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
 
-	// adding values to redis
-	err = client.Set("name", "Elliot", 0).Err()
+	// set json
+	json, err := json.Marshal(Author{Name: "Elliot", Age: 25})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = client.Set("id1234", json, 0).Err()
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println("Setting, Err=", err)
 
-	// getting values
-	val, err := client.Get("name").Result()
+	val, err := client.Get("id1234").Result()
 	if err != nil {
 		fmt.Println(err)
 	}
