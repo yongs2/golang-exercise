95066de50ed5ec3d58af40a0afcff07a1ecc4582
yongs2 <yongs2@hotmail.com>
2020-03-01 21:00:44 +0900

    add various fsm

--- 10.go-redis/22.fsm/example_looplab.go
+++ 10.go-redis/22.fsm/example_looplab.go
@@ -55,24 +55,86 @@ func (d *Door) afterEvent(e *fsm.Event) {
 
 func main() {
 	door := NewDoor("heaven")
+	events := []string{"open", "close", "open", "close"}
+	for _, event := range events {
+		err := door.FSM.Event(event)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	
+	fsm1 := fsm.NewFSM(
+		"E_FS_IDLE",
+		fsm.Events{
+			// MT
+			{Name: "EVT_MT_REQ", Src: []string{"E_FS_IDLE"}, Dst: "E_FS_OPEN"},
+			{Name: "No-EBC-DT", Src: []string{"E_FS_OPEN"}, Dst: "E_FS_DT_OPEN"},
+			{Name: "with-EBC-MT", Src: []string{"E_FS_OPEN"}, Dst: "E_FS_MT_OPEN"},
+			{Name: "EVT_DAA-SUCC", Src: []string{"E_FS_DT_OPEN"}, Dst: "E_FS_DT_WAIT_REPORT"},
+			{Name: "EVT_DAA-TIMEOUT", Src: []string{"E_FS_DT_OPEN"}, Dst: "E_FS_DT_RETRY"},
+			{Name: "EVT_DT-RETRY", Src: []string{"E_FS_DT_RETRY"}, Dst: "E_FS_DT_OPEN"},
+			{Name: "EVT_DNR-SUCC", Src: []string{"E_FS_DT_WAIT_REPORT"}, Dst: "E_FS_DT_WAIT_ECB"},
+			{Name: "EVT_DNR-FAIL", Src: []string{"E_FS_DT_WAIT_REPORT"}, Dst: "E_FS_DT_RETRY"},
+			{Name: "EVT_CDR_ECB", Src: []string{"E_FS_DT_WAIT_ECB"}, Dst: "E_FS_DT_RETRY"},
+			{Name: "No-EBC-Type-B", Src: []string{"E_FS_DT_WAIT_ECB"}, Dst: "E_FS_DT_SUCCESS"},
+			{Name: "No-EBC-Type-O", Src: []string{"E_FS_DT_WAIT_ECB"}, Dst: "E_FS_DT_FAILURE"},
+			{Name: "No-EBC-DT", Src: []string{"E_FS_MT_OPEN"}, Dst: "E_FS_DT_OPEN"},
+			{Name: "EVT_TDA-SUCC", Src: []string{"E_FS_MT_OPEN"}, Dst: "E_FS_MT_SUCCESS"},
+			{Name: "EVT_TDA-TIMEOUT", Src: []string{"E_FS_MT_OPEN"}, Dst: "E_FS_MT_RETRY"},
+			{Name: "EVT_MT-RETRY", Src: []string{"E_FS_MT_RETRY"}, Dst: "E_FS_MT_OPEN"},
+			{Name: "Check-Fail", Src: []string{"E_FS_DT_OPEN", "E_FS_DT_RETRY"}, Dst: "E_FS_DT_FAILURE"},
+			{Name: "Check-Fail", Src: []string{"E_FS_MT_OPEN", "E_FS_MT_RETRY"}, Dst: "E_FS_MT_FAILURE"},
+			{Name: "Close", Src: []string{"E_FS_DT_SUCCESS", "E_FS_DT_FAILURE", "E_FS_MT_SUCCESS", "E_FS_MT_FAILURE"}, Dst: "E_FS_CLOSE"},
 
-	err := door.FSM.Event("open")
-	if err != nil {
-		fmt.Println(err)
+			// MO
+			{Name: "EVT_ODR", Src: []string{"E_FS_IDLE"}, Dst: "E_FS_OPEN"},
+			{Name: "MO-Open", Src: []string{"E_FS_OPEN"}, Dst: "E_FS_MO_OPEN"},			
+			{Name: "EVT_MO-NOTI", Src: []string{"E_FS_MO_OPEN"}, Dst: "E_FS_MO_SUCCESS"},
+			{Name: "EVT_MO-NOTI-TIMEOUT", Src: []string{"E_FS_MO_OPEN"}, Dst: "E_FS_MO_RETRY"},			
+			{Name: "EVT_MO-RETRY", Src: []string{"E_FS_MO_RETRY"}, Dst: "E_FS_MO_OPEN"},
+			{Name: "Check-Fail", Src: []string{"E_FS_OPEN", "E_FS_MO_OPEN", "E_FS_MO_RETRY"}, Dst: "E_FS_MO_FAILURE"},
+			{Name: "Close", Src: []string{"E_FS_MO_SUCCESS", "E_FS_MO_FAILURE"}, Dst: "E_FS_CLOSE"},
+			{Name: "Idle", Src: []string{"E_FS_CLOSE"}, Dst: "E_FS_IDLE"},
+		},
+		fsm.Callbacks{
+			"leave_state":  func(e *fsm.Event) {
+				fmt.Printf("%s >> leaveState.src=%s, dst=%s\n", time.Now().Format("15:04:05.999"), e.Src, e.Dst)
+			},
+			"enter_state":  func(e *fsm.Event) {
+				fmt.Printf("%s >> enterState.src=%s\n", time.Now().Format("15:04:05.999"), e.Src)
+			},
+		},
+	)
+
+	// MO
+	fmt.Println(">> MO ------------")
+	events = []string{
+		"EVT_ODR", "MO-Open", "EVT_MO-NOTI", "Close", "Idle",
+		"EVT_ODR", "Check-Fail", "Close", "Idle",
+		"EVT_ODR", "MO-Open", "Check-Fail", "Close", "Idle",
+		"EVT_ODR", "MO-Open", "EVT_MO-NOTI-TIMEOUT", "Check-Fail", "Close", "Idle",
+		"EVT_ODR", "MO-Open", "EVT_MO-NOTI-TIMEOUT", "EVT_MO-RETRY", "Check-Fail", "Close", "Idle",
+	}
+	for _, event := range events {
+		err := fsm1.Event(event)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 
-	err = door.FSM.Event("close")
-	if err != nil {
-		fmt.Println(err)
+	// MT
+	fmt.Println(">> MT ------------")
+	events = []string{
+		"EVT_MT_REQ", "with-EBC-MT", "EVT_TDA-SUCC", "Close", "Idle",
+		"EVT_MT_REQ", "with-EBC-MT", "Check-Fail", "Close", "Idle",
+		"EVT_MT_REQ", "with-EBC-MT", "EVT_TDA-TIMEOUT", "Check-Fail", "Close", "Idle",
+		"EVT_MT_REQ", "with-EBC-MT", "EVT_TDA-TIMEOUT", "EVT_MT-RETRY", "Check-Fail", "Close", "Idle",
+		"EVT_MT_REQ", "with-EBC-MT", "No-EBC-DT", "EVT_DAA-SUCC", "EVT_DNR-SUCC", "No-EBC-Type-B", "Close", "Idle",
 	}
-
-	err = door.FSM.Event("open")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	err = door.FSM.Event("close")
-	if err != nil {
-		fmt.Println(err)
+	for _, event := range events {
+		err := fsm1.Event(event)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
