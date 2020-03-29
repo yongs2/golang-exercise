688dbb4ee33fdcd63bfbd59f0fb8b9d63e1e123d
yongs2 <yongs2@hotmail.com>
2020-03-29 17:01:37 +0900

    update method overloading

--- 12.golangbyexample/25.polymorphism/example_overloading.go
+++ 12.golangbyexample/25.polymorphism/example_overloading.go
@@ -4,15 +4,59 @@ import (
 	"fmt"
 )
 
-func main() {
-	handle(1, "abc")
-	handle("abc", "xyz", 3)
-	handle(1, 2, 3, 4)
+type person struct {
+	name   string
+	gender string
+	age    int
 }
 
-func handle(params ...interface{}) {
-	fmt.Println("Handle func called with parameters:")
-	for _, param := range params {
-		fmt.Printf("%v\n", param)
+func main() {
+	err := addPerson("Tina", "Female", 20)
+	if err != nil {
+		fmt.Println("PersonAdd Error: " + err.Error())
 	}
+
+	err = addPerson("John", "Male")
+	if err != nil {
+		fmt.Println("PersonAdd Error: " + err.Error())
+	}
+
+	err = addPerson("Wick", 2, 3)
+	if err != nil {
+		fmt.Println("PersonAdd Error: " + err.Error())
+	}
+}
+
+func addPerson(args ...interface{}) error {
+	if len(args) > 3 {
+		return fmt.Errorf("Wront number of arguments passed")
+	}
+	p := &person{}
+	//0 is name, 1 is gender, 2 is age
+	for i, arg := range args {
+		switch i {
+		case 0: //name
+			name, ok := arg.(string)
+			if !ok {
+				return fmt.Errorf("Name is not passed as string")
+			}
+			p.name = name
+		case 1: // gender
+			gender, ok := arg.(string)
+			if !ok {
+				return fmt.Errorf("Gender is not passed as string")
+			}
+			p.gender = gender
+		case 2: // age
+			age, ok := arg.(int)
+			if !ok {
+				return fmt.Errorf("Age is not passed as int")
+			}
+			p.age = age
+		default:
+			return fmt.Errorf("Wrong parametes passed")
+		}
+	}
+	fmt.Printf("Person struct is %+v\n", p)
+	return nil
 }
