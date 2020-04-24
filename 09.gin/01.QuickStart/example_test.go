f4b28ba0d95847e2e43a530070852306e624f141
yongs2 <yongs2@hotmail.com>
2020-04-24 22:46:51 +0900

    fix apitest-jsonpath

--- 09.gin/01.QuickStart/example_test.go
+++ 09.gin/01.QuickStart/example_test.go
@@ -5,23 +5,25 @@ import (
 	"testing"
 
 	"github.com/steinfletcher/apitest"
+	jsonpath "github.com/steinfletcher/apitest-jsonpath"
 )
 
 func TestExample(t *testing.T) {
 	t.Run("Success to ping", func(t *testing.T) {
 		apitest.New().
-			Report(apitest.SequenceDiagram()).
+			//Report(apitest.SequenceDiagram()).
 			Handler(newApp().Router).
 			Get("/ping").
 			Expect(t).
-			Body(`{"message": "pong"}`).
+			Assert(jsonpath.Equal(`$.message`, "pong")).
+			//Body(`{"message": "pong"}`).
 			Status(http.StatusOK).
 			End()
 	})
 
 	t.Run("Fail to ping", func(t *testing.T) {
 		apitest.New().
-			Report(apitest.SequenceDiagram()).
+			//Report(apitest.SequenceDiagram()).
 			Handler(newApp().Router).
 			Get("/pick").
 			Expect(t).
