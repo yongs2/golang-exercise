14d567f019c36564f6a729f1257940f880d45881
yongs2 <yongs2@hotmail.com>
2020-07-04 22:12:27 +0900

    add ParseInput

--- 14.synapsoftQuiz/Quiz2020/calc.go
+++ 14.synapsoftQuiz/Quiz2020/calc.go
@@ -21,25 +21,22 @@ import (
  *
  */
 
+// MaxValue : 최대 값
 const MaxValue int = 39
 
+// 에러 정의
+var ErrNoInput = errors.New("입력값이 없습니다.")
+var ErrInvalidOperator = errors.New("연산자를 잘못 입력하였습니다.")
+var ErrInvalidRomani = errors.New("로마숫자 형식이 잘못되었습니다.")
+
+// Calc : 로마 숫자 계산기
 func Calc(input string) (result string) {
 	var err error
 
-	if len(input) <= 0 {
-		return "입력값이 없습니다."
+	left, operator, right, err := ParseInput(input)
+	if err != nil {
+		return err.Error()
 	}
-
-	// 사칙연산 을 기준으로 값을 구분
-	nOffset := strings.IndexAny(input, "+-*/")
-	if nOffset < 0 {
-		return fmt.Sprintf("잘못 입력하였습니다. offset=%v", nOffset)
-	}
-
-	operator := string(input[nOffset])
-	left := strings.TrimSpace(input[:nOffset])
-	right := strings.TrimSpace(input[nOffset+1:])
-	//log.Printf("op[%v], left[%v], right[%v]", operator, left, right)
 
 	var a, b, c, d int
 	if a, err = ParseRomani(left); err != nil {
@@ -48,6 +45,8 @@ func Calc(input string) (result string) {
 	if b, err = ParseRomani(right); err != nil {
 		return "잘못 입력하였습니다.3"
 	}
+
+	// 사칙연산
 	if operator == "+" {
 		c = a + b
 	} else if operator == "-" {
@@ -77,6 +76,26 @@ func Calc(input string) (result string) {
 	return ConvertRomani(c)
 }
 
+// ParseInput : 로마 숫자 계산식을 사칙연산자를 기준으로 분리
+func ParseInput(input string) (left, operator, right string, err error) {
+	if len(input) <= 0 {
+		return "", "", "", ErrNoInput
+	}
+
+	// 사칙연산 을 기준으로 값을 구분
+	nOffset := strings.IndexAny(input, "+-*/")
+	if nOffset < 0 {
+		return "", "", "", ErrInvalidOperator
+	}
+
+	operator = string(input[nOffset])
+	left = strings.TrimSpace(input[:nOffset])
+	right = strings.TrimSpace(input[nOffset+1:])
+	//log.Printf("op[%v], left[%v], right[%v]", operator, left, right)
+	return left, operator, right, nil
+}
+
+// ParseRomani : 로마 숫자를 integer 로 변환
 func ParseRomani(input string) (result int, err error) {
 	var prevChar byte
 	for _, char := range input {
@@ -86,11 +105,11 @@ func ParseRomani(input string) (result int, err error) {
 			prevChar = 'I'
 		} else if char == 'V' {
 			if prevChar == 'I' {
-				result -= 1
-				result += 4
+				result -= 1 // 이전에 I 에 대해 1로 추가한 부분을 제거
+				result += 4 // IV 인 경우 4로 계산
 				prevChar = ' '
 			} else {
-				result += 5
+				result += 5 // V로 시작하면 5로 계산
 				prevChar = 'V'
 			}
 		} else if char == 'X' {
@@ -106,13 +125,15 @@ func ParseRomani(input string) (result int, err error) {
 	}
 	//log.Printf("result=[%v]", result)
 	if result < 0 {
-		return -1, errors.New("Invalid Romani")
+		return -1, ErrInvalidRomani
 	}
 	return result, nil
 }
 
+// Romani : integer를 로마 숫자로 변환하기 위한 문자열 배열
 var Romani = [11]string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"}
 
+// ConvertRomani : integer를 로마 숫자 문자열로 변환
 func ConvertRomani(input int) (output string) {
 	division := input / 10
 	extra := input % 10
--- 14.synapsoftQuiz/Quiz2020/calc_test.go
+++ 14.synapsoftQuiz/Quiz2020/calc_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 )
 
+// go test -v ./... -coverprofile=coverage.out
+// go tool cover -html=coverage.out -o coverage.html
 func TestParseRomani(t *testing.T) {
 	romaniTests := []struct {
 		input string
@@ -34,28 +36,64 @@ func TestParseRomani(t *testing.T) {
 
 func TestConvertRomani(t *testing.T) {
 	romaniTests := []struct {
-		want  string
 		input int
+		want  string
 	}{
-		{"I", 1},
-		{"II", 2},
-		{"III", 3},
-		{"IV", 4},
-		{"V", 5},
-		{"VI", 6},
-		{"VII", 7},
-		{"VIII", 8},
-		{"IX", 9},
-		{"X", 10},
-		{"XX", 20},
-		{"XXX", 30},
-		{"XXXIX", 39},
+		{1, "I"},
+		{2, "II"},
+		{3, "III"},
+		{4, "IV"},
+		{5, "V"},
+		{6, "VI"},
+		{7, "VII"},
+		{8, "VIII"},
+		{9, "IX"},
+		{10, "X"},
+		{20, "XX"},
+		{30, "XXX"},
+		{39, "XXXIX"},
 	}
 
 	for _, tc := range romaniTests {
 		got := ConvertRomani(tc.input)
 		if got != tc.want {
 			t.Errorf("got %v want %v", got, tc.want)
+		}
+	}
+}
+
+func TestParseInput(t *testing.T) {
+	parseInputTests := []struct {
+		input        string
+		wantLeft     string
+		wantOperator string
+		wantRight    string
+		wantError    error
+	}{
+		{"", "", "", "", ErrNoInput},
+		{"II ^ II", "", "", "", ErrInvalidOperator},
+		{"I + I", "I", "+", "I", nil},
+		{"XX - IV", "XX", "-", "IV", nil},
+		{"III * VI", "III", "*", "VI", nil},
+		{"X / III", "X", "/", "III", nil},
+		{"III / X", "III", "/", "X", nil},
+		{"X - XXX", "X", "-", "XXX", nil},
+		{"XX + XX", "XX", "+", "XX", nil},
+	}
+
+	for _, tc := range parseInputTests {
+		gotLeft, gotOperator, gotRight, gotError := ParseInput(tc.input)
+		if gotLeft != tc.wantLeft {
+			t.Errorf("got %q want %q", gotLeft, tc.wantLeft)
+		}
+		if gotOperator != tc.wantOperator {
+			t.Errorf("got %q want %q", gotOperator, tc.wantOperator)
+		}
+		if gotRight != tc.wantRight {
+			t.Errorf("got %q want %q", gotRight, tc.wantRight)
+		}
+		if gotError != tc.wantError {
+			t.Errorf("got %q want %q", gotError, tc.wantError)
 		}
 	}
 }
