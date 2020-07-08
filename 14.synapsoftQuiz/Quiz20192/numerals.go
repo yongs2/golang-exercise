f9ffc475eacc57a252ca7607359e5287b61d13cc
yongs2 <yongs2@hotmail.com>
2020-07-08 23:14:04 +0900

    2nd commit

--- 14.synapsoftQuiz/Quiz20192/numerals.go
+++ 14.synapsoftQuiz/Quiz20192/numerals.go
@@ -1,5 +1,10 @@
 package numerals
 
+import (
+	"log"
+	"strings"
+)
+
 /* https://www.synapsoft.co.kr/blog/12346
  * korean numerals
  * 80,270원 처럼 금액의 표기는 천 단위로 콤마를 찍지만
@@ -11,9 +16,51 @@ package numerals
  * 모든 금액은 천 단위 구분자인 콤마가 표시돼있고 금액단위인 원
  */
 
+var koreanNum = []string{"", "일", "이", "삼", "사", "오", "육", "칠", "팔", "구", "십"}
+var koreanVal = []string{"", "십", "백", "천", "만", "십만", "백만", "천만", "억", "십억", "백억", "천억", "조", "십조", "백조", "천조"}
+
 func ConvertKoreanNumerals(inputNum string) (korean string) {
-	if inputNum == "1원" {
-		return "일원"
+	korean = ""
+
+	digitStr := strings.Split(inputNum, "원")
+	if len(digitStr) < 2 {
+		return ""
 	}
-	return "에러"
+	log.Printf("digitStr[%v]=[%v]\n", len(digitStr), digitStr[0])
+	idx := 0
+	numIdx := 0
+	bNotOne := false
+	for i := len(digitStr[0]) - 1; i >= 0; i-- {
+		if digitStr[0][i] == ',' {
+			continue
+		}
+		numIdx = int(digitStr[0][i] - '0')
+
+		if numIdx == 0 {
+
+		} else {
+			if numIdx == 1 { // 일
+				if idx > 0 && idx < 5 {
+					bNotOne = true
+				}
+			}
+
+			if (idxs+1)%4 == 0 {
+				korean += " "
+			}
+			if bNotOne == true {
+				korean = koreanVal[idx] + korean
+			} else {
+				korean = koreanNum[numIdx] + koreanVal[idx] + korean
+			}
+
+			//log.Printf("korean[%v], numIdx[%v],val[%v]\n", korean, numIdx, idx)
+		}
+
+		idx++
+		//log.Printf("digit[%v]=[%c]\n", i, digitStr[0][i])
+	}
+
+	korean = korean + "원"
+	return
 }
