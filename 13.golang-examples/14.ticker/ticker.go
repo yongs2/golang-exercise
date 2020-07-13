2594a62bb29f77f0c3bc7b089fae85c99cf58326
yongs2 <yongs2@hotmail.com>
2020-07-13 22:12:46 +0900

    add ticker

---
+++ 13.golang-examples/14.ticker/ticker.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"time"
+)
+
+func getrandom() int {
+	b := make([]byte, 2)
+	rand.Read(b)
+	number := uint32(b[0]) | (uint32(b[1]) << 8)
+	return int(number)
+}
+
+func main() {
+	var x int
+	ticker := time.NewTicker(time.Millisecond * 500)
+	go func() {
+		for t := range ticker.C {
+			fmt.Printf("ticker:\t%d\tat:\t%v\n", x, t)
+		}
+	}()
+
+	go func() {
+		for {
+			x = getrandom()
+		}
+	}()
+
+	time.Sleep(time.Second * 10)
+	ticker.Stop()
+	fmt.Println("Ticker stopped")
+}
