c61c9ecb7c55cfff65102d48293532043f7bce85
yongs2 <yongs2@hotmail.com>
2020-08-19 22:28:36 +0900

    cron example

---
+++ 13.golang-examples/34.cron/cron.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/robfig/cron"
+)
+
+func main() {
+	c := cron.New()
+	MinuteCheck := time.Now().Minute() + 1
+	SpecVal := fmt.Sprintf("%d", MinuteCheck) + " * * * *"
+	fmt.Println("The SpecVal is ", SpecVal)
+	_, err := c.AddFunc(SpecVal, PrintNumber)
+	if err != nil {
+		fmt.Println("Could not register cron - PrintNumber. Error:", err)
+	}
+	c.Start()
+	time.Sleep(time.Second * 70)
+}
+
+func PrintNumber() {
+	fmt.Println("The function was triggered at time:", time.Now())
+}
