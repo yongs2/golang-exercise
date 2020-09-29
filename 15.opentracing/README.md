e0ad98590404119a25cd6079d4f2014d714448e9
yongs2 <yongs2@hotmail.com>
2020-09-29 21:49:34 +0900

    fix test

--- 15.opentracing/README.md
+++ 15.opentracing/README.md
@@ -37,8 +37,23 @@ sysctl -a | grep udp
 
 ```sh
 export JAEGER_SERVICE_NAME=test
-export JAEGER_AGENT_HOST=172.17.0.3
-export JAEGER_AGENT_PORT=6831
 export JAEGER_REPORTER_LOG_SPANS=true
-export JAEGER_ENABLE=true
+export JAEGER_DISABLED=true
+```
+
+- udp로 연동하는 경우는 다음과 같이 설정하나, udp 전송시 1500 bytes 이상인 경우 packet drop 이 발생될 수 있음, 그래서 sidecar 패턴에서만 사용
+
+```sh
+docker inspect jaeger
+export JAEGER_AGENT_HOST=172.17.0.2
+export JAEGER_AGENT_PORT=6831
+```
+
+- http 연동인 경우가 일반적으로 사용함
+
+```sh
+export JAEGER_SAMPLER_TYPE=const
+export JAEGER_SAMPLER_PARAM=1
+export JAEGER_REPORTER_FLUSH_INTERVAL=1s
+export JAEGER_ENDPOINT=http://172.17.0.2:14268/api/traces
 ```
--- 15.opentracing/trivial.go
+++ 15.opentracing/trivial.go
@@ -13,26 +13,34 @@ import (
 
 func main() {
 	// 1) Create a opentracing.Tracer that sends data to Zipkin
-	// cfg := config.Configuration{
-	// 	Sampler: &config.SamplerConfig{
-	// 		Type:	"const",
-	// 		Param:	1,
-	// 	},
-	// 	Reporter: &config.ReporterConfig{
-	// 		LogSpans:		true,
-	// 		BufferFlushInterval:	1 * time.Second,
-	// 	},
+	cfg := config.Configuration{
+		Sampler: &config.SamplerConfig{
+			Type:	"const",
+			Param:	1,
+		},
+		Reporter: &config.ReporterConfig{
+			LogSpans:		true,
+			BufferFlushInterval:	1 * time.Second,
+			CollectorEndpoint: "http://172.17.0.2:14268/api/traces",
+		},
+	}
+	// cfg, err := config.FromEnv()
+	// if err != nil {
+	// 	// parsing errors might happen here, such as when we get a string where we expect a number
+	// 	fmt.Printf("Could not parse Jaeger env vars: %s\n", err.Error())
+	// 	return
 	// }
-	cfg, err := config.FromEnv()
-	if err != nil {
-		// parsing errors might happen here, such as when we get a string where we expect a number
-		fmt.Printf("Could not parse Jaeger env vars: %s\n", err.Error())
-		return
+
+	fmt.Printf("cfg.Sampler=[%v]\n", cfg.Sampler)
+	if cfg.Sampler != nil {
+		fmt.Printf("cfg.Sampler.Type=[%v]\n", cfg.Sampler.Type)
+		fmt.Printf("cfg.Sampler.Param=[%v]\n", cfg.Sampler.Param)
 	}
 
 	fmt.Printf("cfg.Reporter=[%v]\n", cfg.Reporter)
 	if cfg.Reporter != nil {
-		fmt.Printf("cfg.Report.LocalAgentHostPort=[%v]\n", cfg.Reporter.LocalAgentHostPort)
+		fmt.Printf("cfg.Reporter.LocalAgentHostPort=[%v]\n", cfg.Reporter.LocalAgentHostPort)
+		fmt.Printf("cfg.Reporter.CollectorEndpoint=[%v]\n", cfg.Reporter.CollectorEndpoint)
 	}
 	
 
